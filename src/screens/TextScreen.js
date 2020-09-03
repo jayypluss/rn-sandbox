807b610b2989b0cccae03358835305768c24e905
Jonathan F. K <jonathanfk1@gmail.com>
2020-09-02 23:32:40 -0300

    Adding password text input.

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,27 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet, Button, Text, TextInput  } from 'react-native';
 
 const TextScreen = () => {
-
+	const [name, setName] = useState('');
+	const [password, setPassword] = useState('');
 
 	return (
 		<View>
+			<Text style={styles.inputPlaceholder}>Enter name: </Text>
 			<TextInput
 				style={styles.input}
 				autoCapialize="none"
 				autoCorrect={false}
-
+				value={name}
+				onChangeText={(newValue) => {
+					setName(newValue);
+				}}
 			/>
+			<Text style={styles.inputReflect}>My name is: {name}</Text>
+			<Text style={styles.inputPlaceholder}>Enter password: </Text>
+			<TextInput
+				style={styles.input}
+				autoCapialize="none"
+				autoCorrect={false}
+				value={password}
+				onChangeText={(newValue) => {
+					setPassword(newValue);
+				}}
+			/>
+			{ password.length < 7 ?
+				<Text style={styles.inputReflect}>Password lenght must have 6 or more characters.</Text>
+				: null
+			}
+
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	input: {
-		margin: 15,
+		margin: 10,
 		borderColor: 'black',
 		borderWidth: 1,
 		height: 50
+	},
+	inputPlaceholder: {
+		marginTop: 30,
+		marginLeft: 25
+	},
+	inputReflect: {
+		marginLeft: 25
 	}
 });
 
